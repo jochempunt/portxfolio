ac8640053dd456fa4ba22e60403b95ec6837bdc0
jochempunt <jochem.punt@gmail.com>
2025-10-21 23:20:25 +0200

    added more content

--- agreeable-asteroid/src/content.config.ts
+++ agreeable-asteroid/src/content.config.ts
@@ -1,19 +1,29 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
+import { Image } from 'astro:assets'; 
 
 // 2. Import loader(s)
 import { glob, file } from "astro/loaders";
 
 const projectsC = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/projects" }),
-  schema: z.object({
+   schema: ({ image }) => z.object({  // Note: destructure image from the argument
     title: z.string(),
     description: z.string(),
-    img: z.string(),
-    tags:z.array(z.string()),
+    img: image(),  // Now image comes from the schema context
+    tags: z.array(z.string()),
+    prio: z.number(),
+    github: z.string().optional(),
   }),
 });
+
 
 export const collections = {
   projects: projectsC,
 };
+
+
+
+
+
+
