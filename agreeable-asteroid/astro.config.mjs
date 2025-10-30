adfbff0ff68bf5ac040848f3528277281b11b460
jochempunt <jochem.punt@gmail.com>
2025-10-30 22:19:53 +0100

    add sitemap and new domain

--- agreeable-asteroid/astro.config.mjs
+++ agreeable-asteroid/astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  site:'https://jochempunt.com',
+  integrations: [mdx(), sitemap()]
 });
